072a13235cb19ba8ecf7c953cb2150ae4fd2cbff
Frank Yadon <frankyadon@gmail.com>
2025-07-29 22:44:00 -0500

    5th problem solved

--- problems/problem005.py
+++ problems/problem005.py
@@ -1,1 +1,9 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+primes = [2, 3, 5, 7, 11, 13, 17, 19]
+answer = 1
+for num in primes:
+    i = 1
+    while (num ** (i+1)) < 20:
+        i += 1
+    answer = answer * (num ** i)
+print(answer)
