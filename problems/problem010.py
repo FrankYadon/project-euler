d35526823b883f63f903d38c72c240e3e8d752c9
Frank Yadon <frankyadon@gmail.com>
2025-08-03 20:50:32 -0500

    10th problem solved

--- problems/problem010.py
+++ problems/problem010.py
@@ -1,1 +1,11 @@
 # Find the sum of all the primes below two million.
+primes = 0
+start = 2
+end = 2000000
+numbers = [True] * end
+for n in range(2 , end):
+    if numbers[n]:
+        primes += n
+        for comp in range(n*n, end, n):
+            numbers[comp] = False
+print(primes)
